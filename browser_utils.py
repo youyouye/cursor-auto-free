04aa9bd7efaa113c61e128f0c0f2d349f63c8504
panxuezhi <panxuezhi@bytedance.com>
2025-04-14 00:04:02 +0800

    fix

--- browser_utils.py
+++ browser_utils.py
@@ -40,9 +40,9 @@ class BrowserManager:
         if user_agent:
             co.set_user_agent(user_agent)
 
-        co.headless(
-            os.getenv("BROWSER_HEADLESS", "True").lower() == "true"
-        )  # 生产环境使用无头模式
+        # co.headless(
+        #     os.getenv("BROWSER_HEADLESS", "True").lower() == "true"
+        # )  # 生产环境使用无头模式
 
         # Mac 系统特殊处理
         if sys.platform == "darwin":
